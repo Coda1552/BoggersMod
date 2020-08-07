14f3b17ff36c8ca36894f3adc4f602792986cc5c
Cody <cody.koelliker@outlook.com>
2020-08-07 17:15:12 -0600

    Setup

--- src/main/java/mod/coda/boggers/Boggers.java
+++ src/main/java/mod/coda/boggers/Boggers.java
@@ -1,10 +1,32 @@
 package mod.coda.boggers;
 
+import mod.coda.boggers.init.ItemInit;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Boggers.MOD_ID)
 public class Boggers {
 
     public static final String MOD_ID = "boggers";
+    public static Boggers instance;
 
+    public Boggers() {
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        modEventBus.addListener(this::doClientStuff);
+
+        ItemInit.ITEMS.register(modEventBus);
+
+        instance = this;
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLCommonSetupEvent event) {
+    }
 }
---
+++ src/main/java/mod/coda/boggers/init/ItemInit.java
@@ -0,0 +1,13 @@
+package mod.coda.boggers.init;
+
+import mod.coda.boggers.Boggers;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemInit {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, Boggers.MOD_ID);
+
+}
